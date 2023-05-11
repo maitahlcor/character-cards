7f1e46538f295fa3abece05bc109451c15413fde
Andres Lopez <73659007+maitahlcor@users.noreply.github.com>
2023-05-10 19:30:38 -0500

    code depurado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import "./index.css";
-import { useState } from 'react';
-//import {characters as charactersList} from "./assets/data";
 import CharactersList from "./components/CharacterList";
 import characters from "./assets/data.json";
 function App() {
   console.log(characters);
- /* const [characters, setCharacters] = useState(charactersList);
-  const handleAddCharacter = (newCharacter) => {
-    setCharacters([
-      ...characters,
-      newCharacter
-    ])
-  }*/
+
   return (
     <div className="app">
       <h1>Rick and Morty Characters</h1>
